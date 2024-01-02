217528d3714059cdb8c7c1e1922303df38169315
Angel Ponce <angelponcecontacto@gmail.com>
2024-01-02 15:29:05 -0600

    Adding some sql querys and commands to the repository, the sqlite_excercises directory has been made and it contains some basic sql commands

---
+++ .vscode/sqlite_excercises/sql.sql
@@ -0,0 +1,36 @@
+-- In the SQL extension file, were gonna run our commands and querys
+
+-- First we're going to CREATE the Table Users whith 3 columns for each entry
+-- CREATE Table USERS(username TEXT NOT NULL PRIMARY KEY,
+-- password NOT NULL,email NOT NULL);
+
+-- Next were gonna INSERT some entries to the DB
+-- Insert into USERS (username, password,email)
+-- VALUES("Thomas","Thomas1221","ThommyK@gmail.com");
+
+-- The SELECT keyword lets you retrive data from the DB 
+-- select username,email from USERS
+-- WHERE username= "Jake";
+
+-- The WHERE, IN keywords makes it so we can specify which colmns are we interested
+-- and wich values are we looking for exactly.
+-- SELECT email from USERS where username in("Jake","Kevin");
+
+-- The LIKE keyword allows us to do some fuzzy search into the DB
+-- The % is the basis for all the fuzzy search so it will be of great importance 
+-- if theres "J%" then it will look up for eveything that contains J as the first letter
+-- SELECT email FROM USERS WHERE username LIKE("J%");
+
+-- if theres "%mm%" then it will look up for all the entries that have "mm" anywhere in the string
+-- Select email from users where email like ("%mm%");
+
+-- if theres "%m" it will look up for all of the entries that end with m
+-- SELECT email from USERS where email like ("%m");
+
+-- The UPDATE keyword lets us rewrite some information into the DB 
+-- UPDATE USERS SET email = "RewritingEMAIL@email.com", password = "rewritingPassword" WHERE username LIKE ("j%");
+
+-- The DELETE keyword lets us remove entries from the DB
+-- DELETE FROM USERS WHERE username = "Dog";
+
+-- SELECT * from USERS;
--- OOP_essentials/inherithance.py
+++ OOP_essentials/inherithance.py
@@ -6,16 +6,23 @@
 
 class person:
     def __init__(self,Name,Age,School):
-        self.name=Name
+        self._name=Name
         self.age=Age
         self.school=School
 
-    #Method to inherit to child classes
+    # Method to inherit to child classes
     def identify_Yourself():
-        pass
+        pass      
+
+    # Public method to get the private name of the object    
+    def getName(self):
+        return self._name
+    # Public method to set the private name of the object
+    def setName(self,Name):
+        self._name = Name
 
 class student(person):
-    def __init__(self, Name, Age, School, Grade):
+    def __init__(self,Name, Age, School, Grade):
         # parent class init
         super().__init__(Name, Age, School)
         # child class property
@@ -23,28 +30,37 @@ class student(person):
     
     #Polymorphism of the inherited method
     def identify_Yourself(self):
-        return f"{self.name} is a student and studies the {self.grade} grade"
+        return f"{self.getName()} is a student and studies the {self.grade} grade"
     #Own method of the class student
     def studying(self,subject):
-        return f"{self.name} is studying {subject}"
+        return f"{self.getName()} is studying {subject}"
 
 class employee(person):
-    def __init__(self, Name, Age, School, Schedule):
+    def __init__(self,Name, Age, School, Schedule):
         # parent class init
         super().__init__(Name, Age, School)
         # child class property
         self.schedule=Schedule
-
+    #Another Comment 
     #Polymorphism of the inherited method
     def identify_Yourself(self):
-        return f"{self.name} is a employee and works {self.schedule} hours"
+        return f"{self.getName()} is a employee and works {self.schedule} hours"
     #Own method of the class employee
     def working(self,matter):
-        return f"{self.name} is working on {matter}"
+        return f"{self.getName()} is working on {matter}"
     
-student = student("Pedro",20,"UAA","4th")
-employee = employee("Fernando",60,"UAA",8)
+student = student("Jhon",20,"UAA","4th")
+employee = employee("Stuart",60,"UAA",8)
 
+print(student.identify_Yourself())
+print(student.studying("Math"))
+print(employee.identify_Yourself())
+print(employee.working("excel sheets"))
+
+student.setName("Holand")
+employee.setName("Jake")
+
+print("\n~Changed the name of both the student and the employee~\n")
 print(student.identify_Yourself())
 print(student.studying("Math"))
 print(employee.identify_Yourself())
